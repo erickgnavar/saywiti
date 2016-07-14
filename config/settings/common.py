6ac8ee18af3de832de9c7f2fdd7ff252a7e3090d
Erick Navarro <erick@navarro.io>
2016-07-14 12:23:41 -0500

    Setup templates and static

--- config/settings/common.py
+++ config/settings/common.py
@@ -65,7 +65,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'saywiti', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,5 +134,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'saywiti', 'static')
+]
 
 STATIC_URL = '/static/'
